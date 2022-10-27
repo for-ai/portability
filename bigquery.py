d22452298fc7b3e8d72b72b585ad308855eee638
jonas-kgomo <jonaskgmoo@gmail.com>
2022-10-27 15:58:28 +0100

    docs: :zap: adding dirichlet testing
    tensorflow tests

---
+++ bigquery.py
@@ -0,0 +1,13 @@
+# https://console.cloud.google.com/bigquery?project=second-impact-276022
+# tutorial https://codelabs.developers.google.com/codelabs/bigquery-github#1
+
+myquery = 
+[
+  {
+    "name": "repo_name",
+    "mode": "REPEATED",
+    "type": "STRING",
+    "fields": []
+  }
+]
+
