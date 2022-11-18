d2468318d1009bc25ed55f4b5854096876f6e7ae
cowfrica-1702 <63311059+cowfrica-1702@users.noreply.github.com>
2022-11-17 21:22:27 -0500

    add tests

---
+++ src/tensorflow_tests/assertequal_test.py
@@ -0,0 +1,88 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for tensorflow.ops.check_ops."""
+
+import time
+
+import numpy as np
+
+from tensorflow.core.protobuf import config_pb2
+from tensorflow.core.protobuf import rewriter_config_pb2
+from tensorflow.python.client import session
+from tensorflow.python.eager import context
+from tensorflow.python.eager import def_function
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import errors
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import sparse_tensor
+from tensorflow.python.framework import test_util
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import check_ops
+from tensorflow.python.ops import gradients
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import random_ops
+from tensorflow.python.ops.ragged import ragged_factory_ops
+from tensorflow.python.platform import test
+
+
+class AssertEqualTest(test.TestCase):
+
+  @test_util.run_in_graph_and_eager_modes
+  def test_noop_when_both_identical(self):
+    larry = constant_op.constant([])
+    check_op = check_ops.assert_equal(larry, larry)
+    if context.executing_eagerly():
+      self.assertIs(check_op, None)
+    else:
+      self.assertEqual(check_op.type, "NoOp")
+
+
+class EnsureShapeTest(test.TestCase):
+
+  # Static shape inference
+  @test_util.run_deprecated_v1
+  def testStaticShape(self):
+    placeholder = array_ops.placeholder(dtypes.int32)
+    ensure_shape_op = check_ops.ensure_shape(placeholder, (3, 3, 3))
+    self.assertEqual(ensure_shape_op.get_shape(), (3, 3, 3))
+
+  @test_util.run_deprecated_v1
+  def testStaticShape_MergesShapes(self):
+    placeholder = array_ops.placeholder(dtypes.int32, shape=(None, None, 3))
+    ensure_shape_op = check_ops.ensure_shape(placeholder, (5, 4, None))
+    self.assertEqual(ensure_shape_op.get_shape(), (5, 4, 3))
+
+  @test_util.run_deprecated_v1
+  def testStaticShape_RaisesErrorWhenRankIncompatible(self):
+    placeholder = array_ops.placeholder(dtypes.int32, shape=(None, None, 3))
+    with self.assertRaises(ValueError):
+      check_ops.ensure_shape(placeholder, (2, 3))
+
+  @test_util.run_deprecated_v1
+  def testStaticShape_RaisesErrorWhenDimIncompatible(self):
+    placeholder = array_ops.placeholder(dtypes.int32, shape=(None, None, 3))
+    with self.assertRaises(ValueError):
+      check_ops.ensure_shape(placeholder, (2, 2, 4))
+
+  @test_util.run_deprecated_v1
+  def testStaticShape_CanSetUnknownShape(self):
+    placeholder = array_ops.placeholder(dtypes.int32)
+    derived = placeholder / 3
+    ensure_shape_op = check_ops.ensure_shape(derived, None)
+    self.assertEqual(ensure_shape_op.get_shape(), None)
+
+if __name__ == "__main__":
+  test.main()
---
+++ src/tensorflow_tests/get_global_step_test.py
@@ -0,0 +1,92 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Tests for training_util."""
+
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import variables
+from tensorflow.python.platform import test
+from tensorflow.python.training import monitored_session
+from tensorflow.python.training import training_util
+
+
+class GlobalStepTest(test.TestCase):
+
+  def _assert_global_step(self, global_step, expected_dtype=dtypes.int64):
+    self.assertEqual('%s:0' % ops.GraphKeys.GLOBAL_STEP, global_step.name)
+    self.assertEqual(expected_dtype, global_step.dtype.base_dtype)
+    self.assertEqual([], global_step.get_shape().as_list())
+
+  def test_invalid_dtype(self):
+    with ops.Graph().as_default() as g:
+      self.assertIsNone(training_util.get_global_step())
+      variables.VariableV1(
+          0.0,
+          trainable=False,
+          dtype=dtypes.float32,
+          name=ops.GraphKeys.GLOBAL_STEP,
+          collections=[ops.GraphKeys.GLOBAL_STEP])
+      self.assertRaisesRegex(TypeError, 'does not have integer type',
+                             training_util.get_global_step)
+    self.assertRaisesRegex(TypeError, 'does not have integer type',
+                           training_util.get_global_step, g)
+
+  def test_invalid_shape(self):
+    with ops.Graph().as_default() as g:
+      self.assertIsNone(training_util.get_global_step())
+      variables.VariableV1(
+          [0],
+          trainable=False,
+          dtype=dtypes.int32,
+          name=ops.GraphKeys.GLOBAL_STEP,
+          collections=[ops.GraphKeys.GLOBAL_STEP])
+      self.assertRaisesRegex(TypeError, 'not scalar',
+                             training_util.get_global_step)
+    self.assertRaisesRegex(TypeError, 'not scalar',
+                           training_util.get_global_step, g)
+
+  def test_create_global_step(self):
+    self.assertIsNone(training_util.get_global_step())
+    with ops.Graph().as_default() as g:
+      global_step = training_util.create_global_step()
+      self._assert_global_step(global_step)
+      self.assertRaisesRegex(ValueError, 'already exists',
+                             training_util.create_global_step)
+      self.assertRaisesRegex(ValueError, 'already exists',
+                             training_util.create_global_step, g)
+      self._assert_global_step(training_util.create_global_step(ops.Graph()))
+
+  def test_get_global_step(self):
+    with ops.Graph().as_default() as g:
+      self.assertIsNone(training_util.get_global_step())
+      variables.VariableV1(
+          0,
+          trainable=False,
+          dtype=dtypes.int32,
+          name=ops.GraphKeys.GLOBAL_STEP,
+          collections=[ops.GraphKeys.GLOBAL_STEP])
+      self._assert_global_step(
+          training_util.get_global_step(), expected_dtype=dtypes.int32)
+    self._assert_global_step(
+        training_util.get_global_step(g), expected_dtype=dtypes.int32)
+
+  def test_get_or_create_global_step(self):
+    with ops.Graph().as_default() as g:
+      self.assertIsNone(training_util.get_global_step())
+      self._assert_global_step(training_util.get_or_create_global_step())
+      self._assert_global_step(training_util.get_or_create_global_step(g))
+
+if __name__ == '__main__':
+  test.main()
