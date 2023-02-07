d8712fc88c450a7513577854772ccb9c110b7b06
frasermince <frasermince@gmail.com>
2023-02-07 22:32:06 +0000

    Add xla tests.

--- src/pytorch_tests_reduced/xavier_uniform_test.py
+++ src/pytorch_tests_reduced/xavier_uniform_test.py
@@ -48,7 +48,7 @@ from torch.testing._internal.common_utils import freeze_rng_state, run_tests, Te
     TEST_NUMPY, TEST_SCIPY, TEST_WITH_CROSSREF, TEST_WITH_ROCM, \
     download_file, get_function_arglist, load_tests, skipIfMps,\
     TemporaryFileName, TEST_WITH_UBSAN, IS_PPC, \
-    parametrize as parametrize_test, subtest, instantiate_parametrized_tests, IS_WINDOWS
+    parametrize as parametrize_test, subtest, IS_WINDOWS
 from torch.testing._internal.common_cuda import TEST_CUDA, TEST_MULTIGPU, TEST_CUDNN, TEST_CUDNN_VERSION
 from torch.testing._internal.common_nn import NNTestCase, NewModuleTest, CriterionTest, \
     module_tests, criterion_tests, loss_reference_fns, \
@@ -64,6 +64,9 @@ from torch.testing._internal.common_utils import _assertGradAndGradgradChecks, g
 from torch.testing._internal.common_utils import dtype2prec_DONTUSE
 from torch.testing._internal.common_cuda import tf32_on_and_off, tf32_is_not_fp32, tf32_off, tf32_on
 from torch.types import _TensorOrTensors
+
+from ..utils.pytorch_device_decorators import onlyNativeDeviceTypes, onlyAcceleratedDeviceTypes, instantiate_device_type_tests
+from ..utils.timer_wrapper import pytorch_op_timer
 
 
 AMPERE_OR_ROCM = TEST_WITH_ROCM or tf32_is_not_fp32()
@@ -91,20 +94,24 @@ class TestNNInit(TestCase):
         for dims in [0, 1]:
             tensor = self._create_random_nd_tensor(dims, size_min=1, size_max=1)
             with self.assertRaises(ValueError):
-                init.xavier_uniform_(tensor)
+                with pytorch_op_timer():
+                    init.xavier_uniform_(tensor)
 
     @unittest.skipIf(not TEST_SCIPY, "Scipy not found.")
-    def test_xavier_uniform(self):
+    def test_xavier_uniform(self, device):
         for use_gain in [True, False]:
             for dims in [2, 4]:
                 input_tensor = self._create_random_nd_tensor(dims, size_min=20, size_max=25)
+                input_tensor = input_tensor.to(device)
                 gain = 1
 
                 if use_gain:
                     gain = self._random_float(0.1, 2)
-                    init.xavier_uniform_(input_tensor, gain=gain)
+                    with pytorch_op_timer():
+                        init.xavier_uniform_(input_tensor, gain=gain)
                 else:
-                    init.xavier_uniform_(input_tensor)
+                    with pytorch_op_timer():
+                        init.xavier_uniform_(input_tensor)
 
                 fan_in = input_tensor.size(1)
                 fan_out = input_tensor.size(0)
@@ -117,5 +124,6 @@ class TestNNInit(TestCase):
                 assert self._is_uniform(input_tensor, -bounds, bounds)
 
     
+instantiate_device_type_tests(TestNNInit, globals())
 if __name__ == '__main__':
     run_tests()
