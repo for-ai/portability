6b42624d2cdc6d6f6a2adf61e5798367db270bb2
frasermince <frasermince@gmail.com>
2023-02-10 20:26:52 +0000

    Add devices to LSTMCell Test.

--- src/pytorch_tests_reduced/lstmcell_test.py
+++ src/pytorch_tests_reduced/lstmcell_test.py
@@ -45,12 +45,12 @@ from torch.testing._internal.common_utils import freeze_rng_state, run_tests, Te
     TEST_NUMPY, TEST_SCIPY, TEST_WITH_CROSSREF, TEST_WITH_ROCM, \
     download_file, get_function_arglist, load_tests, skipIfMps,\
     TemporaryFileName, TEST_WITH_UBSAN, IS_PPC, \
-    parametrize as parametrize_test, subtest, instantiate_parametrized_tests, IS_WINDOWS
+    parametrize as parametrize_test, subtest, IS_WINDOWS
 from torch.testing._internal.common_cuda import TEST_CUDA, TEST_MULTIGPU, TEST_CUDNN, TEST_CUDNN_VERSION
 from torch.testing._internal.common_nn import NNTestCase, NewModuleTest, CriterionTest, \
     module_tests, criterion_tests, loss_reference_fns, \
     ctcloss_reference, new_module_tests, single_batch_reference_fn
-from torch.testing._internal.common_device_type import instantiate_device_type_tests, dtypes, \
+from torch.testing._internal.common_device_type import dtypes, \
     dtypesIfCUDA, precisionOverride, skipCUDAIfCudnnVersionLessThan, onlyCUDA, onlyCPU, \
     skipCUDAIfRocm, skipCUDAIf, skipCUDAIfNotRocm, \
     onlyNativeDeviceTypes, deviceCountAtLeast, largeTensorTest, expectedFailureMeta, skipMeta, get_all_device_types
@@ -63,6 +63,9 @@ from torch.testing._internal.common_utils import _assertGradAndGradgradChecks, g
 from torch.testing._internal.common_utils import dtype2prec_DONTUSE
 from torch.testing._internal.common_cuda import tf32_on_and_off, tf32_is_not_fp32, tf32_off, tf32_on
 from torch.types import _TensorOrTensors
+
+from ..utils.timer_wrapper import pytorch_op_timer
+from ..utils.pytorch_device_decorators import onlyAcceleratedDeviceTypes, instantiate_device_type_tests
 
 
 AMPERE_OR_ROCM = TEST_WITH_ROCM or tf32_is_not_fp32()
@@ -87,9 +90,10 @@ if TEST_NUMPY:
 class TestNN(NNTestCase):
 
 
-    def test_RNN_cell_no_broadcasting(self):
+    def test_RNN_cell_no_broadcasting(self, device):
         def test(cell_module, input, hx, input_size, hidden_size):
-            cell = cell_module(input_size, hidden_size)
+            with pytorch_op_timer():
+                cell = cell_module(input_size, hidden_size)
             self.assertRaises(RuntimeError, lambda: cell(input, hx))
 
         def test_all(hidden_size, bad_hx, good_hx, input_size, input):
@@ -98,46 +102,49 @@ class TestNN(NNTestCase):
 
         hidden_size = 20
         input_size = 10
-        input = torch.randn(3, input_size)
-        bad_hx = torch.randn(1, hidden_size)
-        good_hx = torch.randn(3, hidden_size)
+        input = torch.randn(3, input_size, device=device)
+        bad_hx = torch.randn(1, hidden_size, device=device)
+        good_hx = torch.randn(3, hidden_size, device=device)
 
         # Test hidden/input batch size broadcasting
         test_all(hidden_size, bad_hx, good_hx, input_size, input)
 
         # Test hx's hidden_size vs module's hidden_size broadcasting
-        bad_hx = torch.randn(3, 1)
+        bad_hx = torch.randn(3, 1, device=device)
         test_all(hidden_size, bad_hx, good_hx, input_size, input)
 
         # Test input's input_size vs module's input_size broadcasting
         bad_input = torch.randn(3, 1)
         test_all(hidden_size, good_hx, good_hx, input_size, bad_input)
 
-    def test_LSTM_cell(self):
+    def test_LSTM_cell(self, device):
         # this is just a smoke test; these modules are implemented through
         # autograd so no Jacobian test is needed
         for bias in (True, False):
-            input = torch.randn(3, 10)
-            hx = torch.randn(3, 20)
-            cx = torch.randn(3, 20)
-            lstm = nn.LSTMCell(10, 20, bias=bias)
+            input = torch.randn(3, 10, device=device)
+            hx = torch.randn(3, 20, device=device)
+            cx = torch.randn(3, 20, device=device)
+            with pytorch_op_timer():
+                lstm = nn.LSTMCell(10, 20, bias=bias, device=device)
             for _ in range(6):
                 hx, cx = lstm(input, (hx, cx))
 
             (hx + cx).sum().backward()
 
-    def test_LSTM_cell_forward_input_size(self):
-        input = torch.randn(3, 11)
-        hx = torch.randn(3, 20)
-        cx = torch.randn(3, 20)
-        lstm = nn.LSTMCell(10, 20)
+    def test_LSTM_cell_forward_input_size(self, device):
+        input = torch.randn(3, 11, device=device)
+        hx = torch.randn(3, 20, device=device)
+        cx = torch.randn(3, 20, device=device)
+        with pytorch_op_timer():
+            lstm = nn.LSTMCell(10, 20, device=device)
         self.assertRaises(Exception, lambda: lstm(input, (hx, cx)))
 
-    def test_LSTM_cell_forward_hidden_size(self):
-        input = torch.randn(3, 10)
-        hx = torch.randn(3, 21)
-        cx = torch.randn(3, 20)
-        lstm = nn.LSTMCell(10, 20)
+    def test_LSTM_cell_forward_hidden_size(self, device):
+        input = torch.randn(3, 10, device=device)
+        hx = torch.randn(3, 21, device=device)
+        cx = torch.randn(3, 20, device=device)
+        with pytorch_op_timer():
+            lstm = nn.LSTMCell(10, 20, device=device)
         self.assertRaises(Exception, lambda: lstm(input, (hx, cx)))
         self.assertRaises(Exception, lambda: lstm(input, (cx, hx)))
 
@@ -145,12 +152,13 @@ class TestNN(NNTestCase):
     
 
 class TestNNDeviceType(NNTestCase):
-    @onlyCUDA
+    @onlyAcceleratedDeviceTypes
     @dtypes(torch.double)
     def test_lstmcell_backward_only_one_output_grad(self, device, dtype):
         # checks that undefined gradients doen't hamper the backward
         # see #11872
-        l = torch.nn.LSTMCell(2, 3).to(device).to(dtype=dtype)
+        with pytorch_op_timer():
+            l = torch.nn.LSTMCell(2, 3).to(device).to(dtype=dtype)
         s = torch.randn(1, 2, device=device, dtype=dtype, requires_grad=True)
         for i in range(2):
             out = l(s)[i]
@@ -159,7 +167,7 @@ class TestNNDeviceType(NNTestCase):
 
     
 instantiate_device_type_tests(TestNNDeviceType, globals())
-instantiate_parametrized_tests(TestNN)
+instantiate_device_type_tests(TestNN, globals())
 
 if __name__ == '__main__':
     run_tests()
