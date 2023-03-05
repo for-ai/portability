fc2a0e4007cdec4acfb5afd1925f9bc8cca7d83d
Fraser Mince <frasermince@Frasers-MacBook-Pro.local>
2023-03-05 17:37:54 -0600

    Add tensorflow custom test case.

--- src/utils/tensorflow_contexts.py
+++ src/utils/tensorflow_contexts.py
@@ -1,5 +1,28 @@
 import tensorflow as tf
 import os
+from tensorflow.python.platform import test
+import contextlib
+
+
+class PortabilityTestCase(test.TestCase):
+    @contextlib.contextmanager
+    def _constrain_devices_and_set_default(self, sess, use_gpu, force_gpu):
+        """Set the session and its graph to global default and constrain devices."""
+        if context.executing_eagerly():
+            yield None
+        else:
+            with sess.graph.as_default(), sess.as_default():
+                # Use the name of an actual device if one is detected, or
+                # '/device:GPU:0' otherwise
+                device_name = self.gpu_device_name()
+                if os.environ['DEVICE'] == "tpu":
+                    device_name = "/device:TPU:0"
+                elif os.environ['DEVICE'] == "tpu":
+                    device_name = "/device:GPU:0"
+                else:
+                    device_name = "/device:CPU:0"
+                with sess.graph.device(device_name):
+                    yield sess
 
 
 def use_environment_device():
