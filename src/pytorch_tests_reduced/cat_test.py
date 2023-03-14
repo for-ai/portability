d10ca3886bdfd4299bfb33a85eb5c97add746e59
cowfrica-1702 <dunganhdinh2002@gmail.com>
2023-03-14 17:49:47 +0000

    move to reduced list and add timer

---
+++ src/pytorch_tests_reduced/cat_test.py
@@ -0,0 +1,327 @@
+# Owner(s): ["module: tensor creation"]
+
+import torch
+
+import random
+from ..utils.pytorch_device_decorators import onlyNativeDeviceTypes, onlyAcceleratedDeviceTypes, instantiate_device_type_tests
+from ..utils.timer_wrapper import pytorch_op_timer
+
+from torch.testing._internal.common_utils import (
+    TestCase, run_tests, slowTest)
+from torch.testing._internal.common_device_type import (
+    onlyCPU, dtypes)
+from torch.testing._internal.common_dtype import (
+    all_types_and_complex_and
+)
+
+
+
+class TestTensorCreation(TestCase):
+    exact_dtype = True
+
+    def test_cat_mem_overlap(self, device):
+        x = torch.rand((1, 3), device=device).expand((6, 3))
+        y = torch.rand((3, 3), device=device)
+        with self.assertRaisesRegex(RuntimeError, 'unsupported operation'):
+            with pytorch_op_timer():
+                torch.cat([y, y], out=x)
+
+    def test_cat_all_dtypes_and_devices(self, device):
+        for dt in all_types_and_complex_and(torch.half, torch.bool, torch.bfloat16, torch.chalf):
+            x = torch.tensor([[1, 2], [3, 4]], dtype=dt, device=device)
+
+            expected1 = torch.tensor(
+                [[1, 2], [3, 4], [1, 2], [3, 4]], dtype=dt, device=device)
+            with pytorch_op_timer():
+                test_1 = torch.cat((x, x), 0)
+            self.assertEqual(test_1, expected1)
+
+            expected2 = torch.tensor(
+                [[1, 2, 1, 2], [3, 4, 3, 4]], dtype=dt, device=device)
+            with pytorch_op_timer():
+                test_2 = torch.cat((x, x), 1)
+            self.assertEqual(test_2, expected2)
+
+    def test_cat_empty_legacy(self, device):
+        # FIXME: this is legacy behavior and should be removed
+        # when we support empty tensors with arbitrary sizes
+        dtype = torch.float32
+
+        x = torch.randn((4, 3, 32, 32), dtype=dtype, device=device)
+        empty = torch.randn((0,), dtype=dtype, device=device)
+        with pytorch_op_timer():
+            res1 = torch.cat([x, empty], dim=1)
+        with pytorch_op_timer():
+            res2 = torch.cat([empty, x], dim=1)
+        self.assertEqual(res1, res2)
+        with pytorch_op_timer():
+            res1 = torch.cat([empty, empty], dim=1)
+        self.assertEqual(res1, empty)
+
+    def test_cat_empty(self, device):
+        dtype = torch.float32
+
+        x = torch.randn((4, 3, 32, 32), dtype=dtype, device=device)
+        empty = torch.randn((4, 0, 32, 32), dtype=dtype, device=device)
+        with pytorch_op_timer():
+            res1 = torch.cat([x, empty], dim=1)
+        with pytorch_op_timer():
+            res2 = torch.cat([empty, x], dim=1)
+        self.assertEqual(res1, res2)
+        with pytorch_op_timer():
+            res1 = torch.cat([empty, empty], dim=1)
+        self.assertEqual(res1, empty)
+
+    @onlyNativeDeviceTypes
+    def test_cat_out(self, device):
+        x = torch.zeros((0), device=device)
+        y = torch.randn((4, 6), device=device)
+
+        w = y.view(-1).clone()
+        with pytorch_op_timer():
+            a = torch.cat([w[:2], w[4:6]])
+        with pytorch_op_timer():
+            b = torch.cat([w[:2], w[4:6]], out=w[6:10])
+        self.assertEqual(a, b)
+        self.assertEqual(w[:6], y.view(-1)[:6])
+
+        # Case:
+        # Reference: https://github.com/pytorch/pytorch/issues/49878
+        for dim in [0, 1]:
+            x = torch.zeros((10, 5, 2), device=device)
+
+            random_length = random.randint(1, 4)
+            y = x.narrow(dim, 0, x.shape[dim] - random_length)
+            val = torch.full_like(y[0], 3., device=device)
+
+            # if dim == 0:
+            #     self.assertTrue(y.is_contiguous())
+            # else:
+            #     self.assertFalse(y.is_contiguous())
+            with pytorch_op_timer():
+                torch.cat((val[None],) * y.shape[0], dim=0, out=y)
+            with pytorch_op_timer():
+                expected_y = torch.cat((val[None],) * y.shape[0], dim=0)
+            expected_x = torch.zeros((10, 5, 2), device=device)
+            if dim == 0:
+                expected_x[:x.shape[dim] - random_length, :, :] = expected_y
+            elif dim == 1:
+                expected_x[:, :x.shape[dim] - random_length, :] = expected_y
+
+            self.assertEqual(y, expected_y)
+            self.assertEqual(x, expected_x)
+
+    def test_cat_out_channels_last(self, device):
+        x = torch.randn((4, 3, 8, 8))
+        y = torch.randn(x.shape)
+        with pytorch_op_timer():
+            res1 = torch.cat((x, y))
+        z = res1.clone().contiguous(memory_format=torch.channels_last)
+        with pytorch_op_timer():
+            res2 = torch.cat((x, y), out=z)
+        self.assertEqual(res1, res2)
+
+    @onlyNativeDeviceTypes
+    def test_cat_in_channels_last(self, device):
+        for dim in range(4):
+            x = torch.randn((4, 15, 8, 8), device=device)
+            y = torch.randn(x.shape, device=device)
+            with pytorch_op_timer():
+                res1 = torch.cat((x, y), dim=dim)
+            x = x.clone().contiguous(memory_format=torch.channels_last)
+            y = y.clone().contiguous(memory_format=torch.channels_last)
+            with pytorch_op_timer():
+                res2 = torch.cat((x, y), dim=dim)
+            self.assertTrue(res2.is_contiguous(
+                memory_format=torch.channels_last))
+            self.assertEqual(res1, res2)
+
+            # Size larger than grain size.
+            x = torch.randn((4, 15, 256, 256), device=device)
+            y = torch.randn(x.shape, device=device)
+            with pytorch_op_timer():
+                res1 = torch.cat((x, y), dim=dim)
+            x = x.clone().contiguous(memory_format=torch.channels_last)
+            y = y.clone().contiguous(memory_format=torch.channels_last)
+            with pytorch_op_timer():
+                res2 = torch.cat((x, y), dim=dim)
+            self.assertTrue(res2.is_contiguous(
+                memory_format=torch.channels_last))
+            self.assertEqual(res1, res2)
+
+    @onlyNativeDeviceTypes
+    def test_cat_preserve_channels_last(self, device):
+        x = torch.randn((4, 3, 8, 8), device=device)
+        y = torch.randn(x.shape, device=device)
+        with pytorch_op_timer():
+            res1 = torch.cat((x, y))
+        with pytorch_op_timer():
+            res2 = torch.cat((x.contiguous(memory_format=torch.channels_last), y.contiguous(
+            memory_format=torch.channels_last)))
+        self.assertEqual(res1, res2)
+        self.assertTrue(res2.is_contiguous(memory_format=torch.channels_last))
+        # discontiguous channels-last inputs
+        x = torch.arange(24, dtype=torch.float, device=device).reshape(
+            2, 2, 3, 2).to(memory_format=torch.channels_last)
+        x1 = x[:, :, :2]
+        x2 = x[:, :, 1:]
+        with pytorch_op_timer():
+            res1 = torch.cat((x1, x2), dim=-1)
+        with pytorch_op_timer():
+            res2 = torch.cat((x1.contiguous(), x2.contiguous()), dim=-1)
+        self.assertEqual(res1, res2)
+        self.assertTrue(res1.is_contiguous(memory_format=torch.channels_last))
+
+    @onlyAcceleratedDeviceTypes
+    def test_cat_out_memory_format(self, device):
+        inp_size = (4, 4, 4, 4)
+        expected_size = (8, 4, 4, 4)
+        a_cuda = torch.randn(inp_size, device=device).contiguous(
+            memory_format=torch.channels_last)
+        a_cpu = torch.randn(inp_size, device='cpu').contiguous(
+            memory_format=torch.channels_last)
+        b_cuda = torch.randn(inp_size, device=device).contiguous(
+            memory_format=torch.contiguous_format)
+        b_cpu = torch.randn(inp_size, device='cpu').contiguous(
+            memory_format=torch.contiguous_format)
+        c_cuda = torch.randn(inp_size, device=device).contiguous(
+            memory_format=torch.channels_last)
+
+        # Case 1: if out= is the correct shape then the memory format of out= is respected
+
+        out_cuda = torch.empty(expected_size, device=device).contiguous(
+            memory_format=torch.contiguous_format)
+        with pytorch_op_timer():
+            res1_cuda = torch.cat((a_cuda, b_cuda), out=out_cuda)
+
+        out_cpu = torch.empty(expected_size, device='cpu').contiguous(
+            memory_format=torch.contiguous_format)
+        with pytorch_op_timer():
+            res1_cpu = torch.cat((a_cpu, b_cpu), out=out_cpu)
+
+        self.assertTrue(res1_cuda.is_contiguous(
+            memory_format=torch.contiguous_format))
+        self.assertTrue(res1_cpu.is_contiguous(
+            memory_format=torch.contiguous_format))
+
+        # Case 2: if out= is not the correct shape then the output it is resized internally
+        # - For both CPU and CUDA variants, it only propagates memory format if all the tensors have
+        #   the same memory format, otherwise it just uses contiguous_format as a default
+
+        out_cuda = torch.empty((0), device=device).contiguous(
+            memory_format=torch.contiguous_format)
+        # a_cuda and b_cuda have different memory_format
+        with pytorch_op_timer():
+            res2_cuda = torch.cat((a_cuda, b_cuda), out=out_cuda)
+
+        out_cpu = torch.empty((0), device='cpu').contiguous(
+            memory_format=torch.contiguous_format)
+        with pytorch_op_timer():
+            res2_cpu = torch.cat((a_cpu, b_cpu), out=out_cpu)
+
+        self.assertTrue(res2_cuda.is_contiguous(
+            memory_format=torch.contiguous_format))
+        self.assertTrue(res2_cpu.is_contiguous(
+            memory_format=torch.contiguous_format))
+
+        out_cuda = torch.empty((0), device=device).contiguous(
+            memory_format=torch.contiguous_format)
+        # a_cuda and c_cuda have same memory_format
+        with pytorch_op_timer():
+            res3_cuda = torch.cat((a_cuda, c_cuda), out=out_cuda)
+
+        self.assertTrue(res3_cuda.is_contiguous(
+            memory_format=torch.channels_last))
+
+    @onlyAcceleratedDeviceTypes
+    def test_cat_stack_cross_devices(self, device):
+        cuda = torch.randn((3, 3), device=device)
+        cpu = torch.randn((3, 3), device='cpu')
+
+        # Stack
+        with self.assertRaisesRegex(RuntimeError,
+                                    "Expected all tensors to be on the same device"):
+            torch.stack((cuda, cpu))
+        with self.assertRaisesRegex(RuntimeError,
+                                    "Expected all tensors to be on the same device"):
+            torch.stack((cpu, cuda))
+
+    # TODO: reconcile with other cat tests
+    # TODO: Compare with a NumPy reference instead of CPU
+    @onlyAcceleratedDeviceTypes
+    def test_cat(self, device):
+        SIZE = 10
+        for dim in range(-3, 3):
+            pos_dim = dim if dim >= 0 else 3 + dim
+            x = torch.rand(13, SIZE, SIZE, device=device).transpose(0, pos_dim)
+            y = torch.rand(17, SIZE, SIZE, device=device).transpose(0, pos_dim)
+            z = torch.rand(19, SIZE, SIZE, device=device).transpose(0, pos_dim)
+            with pytorch_op_timer():
+                res1 = torch.cat((x, y, z), dim)
+            self.assertEqual(res1.narrow(pos_dim, 0, 13), x, atol=0, rtol=0)
+            self.assertEqual(res1.narrow(pos_dim, 13, 17), y, atol=0, rtol=0)
+            self.assertEqual(res1.narrow(pos_dim, 30, 19), z, atol=0, rtol=0)
+
+        x = torch.randn(20, SIZE, SIZE, device=device)
+        with pytorch_op_timer():
+            self.assertEqual(torch.cat(torch.split(x, 7)), x)
+        with pytorch_op_timer():
+            self.assertEqual(torch.cat(torch.chunk(x, 7)), x)
+
+        y = torch.randn(1, SIZE, SIZE, device=device)
+        with pytorch_op_timer():
+            z = torch.cat([x, y])
+        self.assertEqual(z.size(), (21, SIZE, SIZE))
+
+    @slowTest
+    @onlyCPU
+    def test_cat_big(self, device):
+        SIZE1 = 6500
+        SIZE2 = 4500
+        concat_list = []
+        concat_list.append(torch.ones((SIZE1, 1024 * 512),
+                           dtype=torch.uint8, device=device))
+        concat_list.append(torch.ones((SIZE2, 1024 * 512),
+                           dtype=torch.uint8, device=device))
+        with pytorch_op_timer():
+            result = torch.cat(concat_list)
+        self.assertEqual(result.size(0), SIZE1 + SIZE2)
+
+    @onlyCPU
+    @dtypes(torch.half, torch.double, torch.int)
+    def test_cat2(self, device, dtype):
+        SIZE = 10
+        for dim in range(-3, 3):
+            pos_dim = dim if dim >= 0 else 3 + dim
+            x = torch.randint(low=-100, high=100, size=(13, SIZE, SIZE),
+                              device=device).to(dtype).transpose(0, pos_dim)
+            y = torch.randint(low=-100, high=100, size=(17, SIZE, SIZE),
+                              device=device).to(dtype).transpose(0, pos_dim)
+            z = torch.randint(low=-100, high=100, size=(19, SIZE, SIZE),
+                              device=device).to(dtype).transpose(0, pos_dim)
+            with pytorch_op_timer():
+                res1 = torch.cat((x, y, z), dim)
+            self.assertEqual(res1.narrow(pos_dim, 0, 13), x, atol=0, rtol=0)
+            self.assertEqual(res1.narrow(pos_dim, 13, 17), y, atol=0, rtol=0)
+            self.assertEqual(res1.narrow(pos_dim, 30, 19), z, atol=0, rtol=0)
+
+        x = torch.randint(low=-100, high=100, size=(20, SIZE,
+                          SIZE), device=device).to(dtype)
+        with pytorch_op_timer():
+            test_1 = torch.cat(torch.split(x, 7))
+        self.assertEqual(test_1, x)
+        with pytorch_op_timer():
+            test_2 = torch.cat(torch.chunk(x, 7))
+        self.assertEqual(test_2, x)
+
+        y = torch.randint(low=-100, high=100, size=(1, SIZE,
+                          SIZE), device=device).to(dtype)
+        with pytorch_op_timer():
+            z = torch.cat([x, y])
+        self.assertEqual(z.size(), (21, SIZE, SIZE))
+
+
+instantiate_device_type_tests(TestTensorCreation, globals())
+
+if __name__ == '__main__':
+    run_tests()
