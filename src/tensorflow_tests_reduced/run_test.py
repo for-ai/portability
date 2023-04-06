be74446148f82fff4a6671577727a1946959c7ec
frasermince <frasermince@gmail.com>
2023-04-06 15:54:34 +0000

    Remove tests that are intended to only work on cpu

--- src/tensorflow_tests_reduced/run_test.py
+++ src/tensorflow_tests_reduced/run_test.py
@@ -1573,55 +1573,6 @@ class SessionTest(test_util.TensorFlowTestCase):
         with self.assertRaisesRegex(TypeError, 'Cannot interpret feed_dict'):
           sess.run(a, feed_dict={'a': [2.0]})
 
-  def testPerStepTrace(self):
-    run_options = config_pb2.RunOptions(
-        trace_level=config_pb2.RunOptions.SOFTWARE_TRACE)
-    run_metadata = config_pb2.RunMetadata()
-
-    with ops.device('/cpu:0'):
-      with session.Session() as sess:
-        with tensorflow_op_timer():
-          sess.run(constant_op.constant(1.0))
-        self.assertFalse(run_metadata.HasField('step_stats'))
-        with tensorflow_op_timer():
-          sess.run(constant_op.constant(1.0), run_metadata=run_metadata)
-        self.assertFalse(run_metadata.HasField('step_stats'))
-        with tensorflow_op_timer():
-          sess.run(
-            constant_op.constant(1.0),
-            options=run_options,
-            run_metadata=run_metadata)
-
-        self.assertTrue(run_metadata.HasField('step_stats'))
-        self.assertEqual(len(run_metadata.step_stats.dev_stats), 1)
-
-  def testRunOptionsRunMetadata(self):
-    run_options = config_pb2.RunOptions(
-        trace_level=config_pb2.RunOptions.SOFTWARE_TRACE)
-    run_metadata = config_pb2.RunMetadata()
-
-    with ops.device('/cpu:0'):
-      with session.Session() as sess:
-        # all combinations are valid
-        with tensorflow_op_timer():
-          sess.run(constant_op.constant(1.0), options=None, run_metadata=None)
-        with tensorflow_op_timer():
-          sess.run(
-            constant_op.constant(1.0), options=None, run_metadata=run_metadata)
-        self.assertFalse(run_metadata.HasField('step_stats'))
-        with tensorflow_op_timer():
-          sess.run(
-            constant_op.constant(1.0), options=run_options, run_metadata=None)
-        self.assertFalse(run_metadata.HasField('step_stats'))
-        with tensorflow_op_timer():
-          sess.run(
-            constant_op.constant(1.0),
-            options=run_options,
-            run_metadata=run_metadata)
-
-        self.assertTrue(run_metadata.HasField('step_stats'))
-        self.assertEqual(len(run_metadata.step_stats.dev_stats), 1)
-
   def testFeedShapeCompatibility(self):
     with session.Session() as sess:
       with device_context():
